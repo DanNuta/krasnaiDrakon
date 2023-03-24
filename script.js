6b07ad29e71ef4c4609f5b7f27b46d9ce0fa9475
DanNuta <dany.condurari@gmail.com>
2023-03-24 23:05:20 +0200

    commit

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 import {items} from "./constants/items.js";
+import { navDesktop, navTablet } from "./constants/items.js";
 
-console.log(items)
+
+const tabletNav = document.querySelector(".nav_tablet");
+const desktopNav = document.querySelector(".nav_desktop");
+
+navTablet.forEach(item => {
+   tabletNav.append(healperHtmlNav(item));
+});
+
+navDesktop.forEach(item => {
+    desktopNav.append(healperHtmlNav(item));
+});
+
+
+
+
+function healperHtmlNav(target){
+    const ul = document.createElement("ul");
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = `#${target}`;
+    a.textContent = `${target}`;
+
+    li.append(a);
+    ul.append(li);
+    return ul;
+}
